4b7c0cda26d09394d42b294b724a0ecc1906015c
Eduardo Buenadicha <e.b.moya@gmail.com>
2014-10-20 20:10:48 +0200

    jijiji

--- functions/listador.php
+++ functions/listador.php
@@ -63,7 +63,7 @@ if(($desde)&&(!$hasta)){$options .=" AND codigo >= $desde";};
 if($detalles){$options .=" AND detalles LIKE '%$detalles%'";};
 if($comentarios){$options .=" AND comentarios LIKE '%$comentarios%'";};
 //if($refp){$options .=" AND id_proveedor IN (SELECT id FROM proveedores WHERE nombre LIKE '%$refp%')";};
-//if($refp){$options .=" AND refprov LIKE '%$refp%'";};
+if($refp){$options .=" AND refprov LIKE '%$refp%'";};
 
 
 
